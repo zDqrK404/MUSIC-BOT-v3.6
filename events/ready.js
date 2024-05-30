872b0d8a6c35b7a07674641a456b8ac844be48fa
Aladin <alaminlikhon283@gmail.com>
2024-05-30 22:48:27 +0600

    Update ready.js
    activity status changed

--- events/ready.js
+++ events/ready.js
@@ -44,7 +44,7 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
 setInterval(() => client.user.setActivity({ 
-  name: `Update v3.6`, 
+  name: `Update v3.0`, 
   type: ActivityType.Watching }), 10000);
 client.errorLog = config.errorLog
 } else {
