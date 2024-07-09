4f714b395b73a7c8739ac61c493e3f6c26f94638
zDqrK404 <rowshonsaraf404@gmail.com>
2024-07-09 16:50:03 +0600

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ module.exports = {
   ownerID: ["1004206704994566164", ""],
   botInvite: "",
   supportServer: "",
-  mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
+  mongodbURL: "",
   status: 'GlaceYT',
   commandsDir: './commands',
   language: "en",
